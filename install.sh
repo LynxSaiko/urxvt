eea968282793de3af3fcbf6f4a64a0f5e0c85cbc
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-20 03:14:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,37 +27,57 @@ install_package() {
   tar -xvf "$PACKAGE_TAR"
   cd "$PACKAGE_NAME"
 
-  # Menyiapkan dan mengonfigurasi build
-  echo "Mengonfigurasi $PACKAGE_NAME..."
-  ./configure --prefix="$INSTALL_DIR"
+  # Terapkan patch opsional (jika ada)
+  if [ -f "../glib-2.72.3-skip_warnings-1.patch" ]; then
+    echo "Menerapkan patch untuk menghapus peringatan..."
+    patch -Np1 -i "../glib-2.72.3-skip_warnings-1.patch"
+  fi
+
+  # Menyusun dengan Meson dan Ninja
+  echo "Menyiapkan dan mengonfigurasi $PACKAGE_NAME menggunakan Meson..."
+  mkdir build
+  cd build
+  meson --prefix="$INSTALL_DIR" \
+        --buildtype=release \
+        -Dman=true \
+        .. \
+        && ninja
+
   if [ $? -ne 0 ]; then
-    echo "Gagal mengonfigurasi $PACKAGE_NAME"
+    echo "Gagal mengonfigurasi dan membangun $PACKAGE_NAME dengan Meson dan Ninja"
     exit 1
   fi
 
-  # Membangun dan menginstal
-  echo "Membangun dan menginstal $PACKAGE_NAME..."
-  make -j$(nproc) && sudo make install
-  if [ $? -ne 0 ]; then
-    echo "Gagal menginstal $PACKAGE_NAME"
-    exit 1
-  fi
+  # Menginstal dengan Ninja
+  echo "Menginstal $PACKAGE_NAME..."
+  sudo ninja install
+
+  # Salin dokumentasi
+  mkdir -p /usr/share/doc/glib-2.72.3
+  cp -r ../docs/reference/{gio,glib,gobject} /usr/share/doc/glib-2.72.3
 
   # Kembali ke direktori unduhan
   cd "$DOWNLOAD_DIR"
 }
 
-# Mengunduh dan menginstal glib-2.0
-install_package "glib-2.0" "https://ftp.gnome.org/pub/gnome/sources/glib/2.68/glib-2.68.4.tar.xz"
+# Cek apakah file patch ada di direktori
+if [ ! -f "../glib-2.72.3-skip_warnings-1.patch" ]; then
+  echo "File patch tidak ditemukan. Anda dapat mengunduhnya jika perlu."
+fi
 
-# Mengunduh dan menginstal gobject-2.0 (termasuk dalam glib)
-# Tidak perlu mengunduh terpisah, karena bagian dari glib-2.0
+# URL yang benar untuk mengunduh GLib 2.72.3
+GLIB_URL="https://download.gnome.org/sources/glib/2.72/glib-2.72.3.tar.xz"
+GOBJECT_INTROSPECTION_URL="https://download.gnome.org/sources/gobject-introspection/1.72/gobject-introspection-1.72.0.tar.xz"
+WEBKITGTK_URL="https://webkitgtk.org/releases/webkitgtk-2.36.7.tar.xz"
 
-# Mengunduh dan menginstal gthread-2.0 (termasuk dalam glib)
-# Tidak perlu mengunduh terpisah, karena bagian dari glib-2.0
+# Mengunduh dan menginstal GLib 2.72.3
+install_package "glib-2.72.3" "$GLIB_URL"
 
-# Mengunduh dan menginstal webkit-1.0
-install_package "webkit-1.0" "https://github.com/WebKit/webkitgtk/archive/refs/tags/1.0.tar.gz"
+# Mengunduh dan menginstal GObject Introspection 1.72.0
+install_package "gobject-introspection-1.72.0" "$GOBJECT_INTROSPECTION_URL"
+
+# Mengunduh dan menginstal WebKitGTK 2.36.7
+install_package "webkitgtk-2.36.7" "$WEBKITGTK_URL"
 
 # Verifikasi instalasi
 echo "Verifikasi instalasi GLib..."
@@ -65,7 +85,10 @@ pkg-config --modversion glib-2.0
 pkg-config --modversion gobject-2.0
 pkg-config --modversion gthread-2.0
 
-echo "Verifikasi instalasi WebKit..."
-pkg-config --modversion webkitgtk-1.0
+echo "Verifikasi instalasi GObject Introspection..."
+pkg-config --modversion gobject-introspection-1.0
+
+echo "Verifikasi instalasi WebKitGTK..."
+pkg-config --modversion webkit2gtk-4.0
 
 echo "Instalasi selesai!"
