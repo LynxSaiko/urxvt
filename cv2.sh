6c4b4945ab811a90cf9d9641f2b78beb773baaee
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-20 11:13:11 +0700

    Update cv2.sh

--- cv2.sh
+++ cv2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget --no-check-certificate https://lfs.it-privat.dk/patches/downloads/gst-plugins-base/gst-plugins-base-0.10.36-gcc_4_9_0_i686-1.patch
-patch -Np1 -i gst-plugins-base-0.10.36-gcc_4_9_0_i686-1.patch
+wget --no-check-certificate https://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/media-libs/gst-plugins-base/files/gst-plugins-base-0.10.36-glib-2.32.patch?revision=1.2 -O glib2.32.patch
+patch -p1 < glib2.32.patch
 ./configure --prefix=/usr --disable-static &&
 make -j$(nproc)
 make install
