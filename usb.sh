b2ae220228bd2267f651a1c3aadc89e01889a0ce
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-21 03:17:07 +0700

    Create usb.sh

---
+++ usb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./configure --prefix=/usr --datadir=/usr/share/hwdata &&
+make
+make install
+install -dm755 /usr/share/hwdata/ &&
+wget http://www.linux-usb.org/usb.ids -O /usr/share/hwdata/usb.ids
+cat > /etc/cron.weekly/update-usbids.sh << "EOF" &&
+#!/bin/bash
+/usr/bin/wget http://www.linux-usb.org/usb.ids -O /usr/share/hwdata/usb.ids
+EOF
+chmod 754 /etc/cron.weekly/update-usbids.sh
