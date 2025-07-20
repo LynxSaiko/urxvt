1911c7ba8a0259707b23432f2a06c7e1b00971f8
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-20 09:09:42 +0700

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,74 +1,31 @@
 #!/bin/bash
-package_name=""
-package_ext=""
 
-begin() {
-	package_name=$1
-	package_ext=$2
+wget --no-check-certificate https://download.gnome.org/sources/libsoup/2.74/libsoup-2.74.2.tar.xz
+wget --no-check-certificate https://download.gnome.org/sources/glib-networking/2.72/glib-networking-2.72.2.tar.xz
+wget --no-check-certificate https://github.com/rockdaboot/libpsl/releases/download/0.21.1/libpsl-0.21.1.tar.gz
 
-	echo "Starting build of $package_name at $(date)"
-
-	tar xf $package_name.$package_ext
-	cd $package_name
-}
-
-finish() {
-	echo "Finishing build of $package_name at $(date)"
-
-	cd /download1
-	rm -rf $package_name
-}
-cd /download1
-begin atk-2.38.0 tar.xz
-cd atk-2.38.0
-mkdir build &&
-cd    build &&
-
-meson --prefix=/usr --buildtype=release .. &&
-ninja
-finish 
-
-echo "[*] Build Shared Mime [*]"
-begin shared-mime-info-2.2 tar.xz
-cd shared-mime-info-2.2
-tar -xf xdgmime.tar.xz &&
-make -C xdgmime
-mkdir build &&
-cd    build &&
-
-meson --prefix=/usr --buildtype=release -Dupdate-mimedb=true .. &&
-ninja
-ninja install
-finish
-
-echo "[*] Build Gdk Pixbuf [*]"
-begin gdk-pixbuf-2.42.9 tar.xz
-cd gdk-pixbuf-2.42.9
-mkdir build &&
-cd    build &&
-meson --prefix=/usr --buildtype=release --wrap-mode=nofallback .. &&
-ninja
-finish
-
-
-echo "[*] Build Gtk+2 [*]"
-begin gtk+-2.24.33 tar.xz
-cd tk+-2.24.33
-sed -e 's#l \(gtk-.*\).sgml#& -o \1#' \
-    -i docs/{faq,tutorial}/Makefile.in      &&
-
-./configure --prefix=/usr --sysconfdir=/etc &&
-
-make
+tar -xf libpsl-0.21.1.tar.gz
+cd libpsl-0.21.1
+sed -i 's/env python/&3/' src/psl-make-dafsa &&
+./configure --prefix=/usr --disable-static       &&
+make -j$(nproc)
 make install
-gtk-query-immodules-2.0 --update-cache
-cat > ~/.gtkrc-2.0 << "EOF"
-include "/usr/share/themes/Glider/gtk-2.0/gtkrc"
-gtk-icon-theme-name = "hicolor"
-EOF
 
-cat > /etc/gtk-2.0/gtkrc << "EOF"
-include "/usr/share/themes/Clearlooks/gtk-2.0/gtkrc"
-gtk-icon-theme-name = "elementary"
-EOF
-finish
+tar -xf glib-networking-2.72.2.tar.xz
+cd glib-networking-2.72.2
+mkdir build &&
+cd    build &&
+
+meson --prefix=/usr       \
+      --buildtype=release &&
+ninja -j$(nproc)
+ninja install
+
+
+
+tar -xf libsoup-2.74.2.tar.xz
+cd libsoup-2.74.2
+sed -i 's/env python/&3/' src/psl-make-dafsa &&
+./configure --prefix=/usr --disable-static       &&
+make -j$(nproc)
+make install
